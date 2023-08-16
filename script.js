1a3b3ae83573e382b7706fe969d89e382c2423ce
Sangram <sangram1677@gmail.com>
2023-08-16 14:48:31 +0530

    date and time

--- script.js
+++ script.js
@@ -40,4 +40,23 @@ let message1 = `This is my
 first Message for ${lastName}`;
 console.log(message1);
 
+// Date and Time - Data objects are created with new date()
+let date = new Date();
+console.log(date);
+
+let date2 = new Date('June 20 1998 07:15');
+console.log(date2);
+
+let date3 = new Date(1998, 5, 20, 7);
+console.log(date3);
+
+// getter
+
+console.log(date2.getMilliseconds());
+
+// setter
+
+console.log(date3.setFullYear(1999));
+
+
 
